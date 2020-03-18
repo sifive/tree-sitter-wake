895bf04e28156ab4fdd0a00f8145f0e4a7a802ff
Wesley W. Terpstra <wesley@sifive.com>
2020-03-18 13:40:51 -0700

    tree-sitter-wake: actually implement the parser+scanner

--- grammar.js
+++ grammar.js
@@ -1,20 +1,308 @@
-const rules = {
-  source_file: $ => repeat($.description),
-  simple_identifier: $ => /[a-zA-Z_]\w*/,
-  description: $ => choice(),
-  comment: $ => token(choice(
-    seq('//', /.*/),
-    seq(
-      '/*',
-      /[^*]*\*+([^/*][^*]*\*+)*/,
-      '/'
-    )
-  ))
-};
+function binary(op, expr) {
+  return seq(field('lhs', expr), field('operator', op), field('rhs', expr))
+}
+
+function left (n, op, expr) { return prec.left (n, binary(op, expr)) }
+function right(n, op, expr) { return prec.right(n, binary(op, expr)) }
+
+function prefix (n, op, expr) { return prec(n, seq(field('operator', op), field('operand', expr))) }
+function postfix(n, op, expr) { return prec(n, seq(field('operand', expr), field('operator', op))) }
+
+function unary_ops($, expr) {
+  // !!! add all operators
+  return choice(
+    prefix (6, $.mul_op,   expr),
+    prefix (4, $.plus_op,  expr),
+    postfix(2, $.comma_op, expr))
+}
+
+function binary_ops($, expr) {
+  return choice(
+    left (5, $.mul_op,   expr),
+    left (3, $.plus_op,  expr),
+    right(1, $.comma_op, expr))
+}
 
 module.exports = grammar({
   name: 'wake',
-  word: $ => $.simple_identifier,
-  rules: rules,
-  extras: $ => [/\s/, $.comment]
+
+  word: $ => $.low_identifier,
+  extras: $ => [ /[ \t\xa0\u1680\u2000-\u200a\u202f\u205f\u2029]/, $._eol ],
+
+  externals: $ => [
+    $._eol,
+    $._indent,
+    $._dedent
+  ],
+
+  rules: {
+    source_file: $ => repeat(choice(
+      $.tuple,
+      $.data,
+      $.topic,
+      $.publish,
+      $.top_definition,
+      $.top_target,
+      $.package,
+      $.import,
+      $.export)),
+
+    // Types --------------------------------------------------------------------------
+
+    // !!! COLON
+    _full_type: $ => choice(
+      $._term_type,
+      $.application_type,
+      $.binary_type,
+      $.unary_type),
+
+    _term_type: $ => choice(
+      seq('(', $._full_type, ')'),
+      $._identifier),
+
+    term_types: $ => repeat1($._term_type),
+
+    application_type: $ => seq($.high_identifier, repeat1($._term_type)),
+    unary_type:  $ => unary_ops ($, $._full_type),
+    binary_type: $ => binary_ops($, $._full_type),
+
+    // Patterns -----------------------------------------------------------------------
+
+    _full_pattern: $ => choice(
+      $._term_pattern,
+      $.application_pattern,
+      $.binary_pattern,
+      $.unary_pattern,
+      $.hole),
+
+    _def_pattern: $ => choice(
+      $._full_pattern,
+      $.low_application_pattern),
+
+    _term_pattern: $ => choice(
+      seq('(', $._full_pattern, ')'),
+      $._identifier,
+      $._literal),
+
+    term_patterns: $ => repeat1($._term_pattern),
+
+    application_pattern: $ => seq($.high_identifier, repeat1($._term_pattern)),
+    low_application_pattern: $ => seq($.low_identifier, repeat1($._term_pattern)),
+    unary_pattern:  $ => unary_ops ($, $._full_pattern),
+    binary_pattern: $ => binary_ops($, $._full_pattern),
+
+    // Single line expressions --------------------------------------------------------
+
+    _full_expression: $ => choice(
+      $._operator_expression,
+      $.lambda_expression,
+      $.if_expression),
+
+    // !!! COLON
+    _operator_expression: $ => choice(
+      $._term_expression,
+      $.application_expression,
+      $.binary_expression,
+      $.unary_expression,
+      $.prim_expression,
+      $.subscribe_expression,
+      $.match_expression),
+
+    _term_expression: $ => choice(
+      seq('(', $.block_expression, ')'),
+      $._identifier,
+      $._literal,
+      alias($.dot_expression, $.binary_expression)),
+
+    lambda_expression: $ => seq(
+      '\\',
+      field('pattern', $._term_pattern),
+      field('expression', $._full_expression)),
+
+    if_expression: $ => seq(
+      'if',   field('condition', $.block_expression),
+      'then', field('then',      $.block_expression),
+      'else', field('else',      $.block_expression)),
+
+    application_expression: $ => seq($._term_expression, repeat1($._term_expression)),
+    unary_expression:  $ => unary_ops ($, $._operator_expression),
+    binary_expression: $ => binary_ops($, $._operator_expression),
+    dot_expression: $ => left(3, $.dot_op, $._term_expression),
+
+    prim_expression: $ => seq('prim', $.string),
+    subscribe_expression: $ => seq('subscribe', $.low_identifier),
+
+    _block_body: $ => seq(
+      field('preamble', repeat(choice($.definition, $.import, $.target))),
+      field('expression', $._full_expression)),
+
+    block_expression: $ => choice(
+      field('expression', $._full_expression),
+      seq($._indent, $._eol, $._block_body, $._dedent)),
+
+    // Match expression ---------------------------------------------------------------
+
+    // !!! multimatch
+    match_expression: $ => seq(
+      'match',
+      field('arg', $._term_expression),
+      $._indent,
+      field('rules', $.match_rules),
+      $._dedent),
+
+    match_rules: $ => repeat1($.match_rule),
+
+    match_rule: $ => seq(
+      $._eol,
+      field('pattern', $._full_pattern),
+      field('guard', optional(alias($.match_guard, $.block_expression))),
+      '=',
+      field('block', $.block_expression)),
+
+    match_guard: $ => choice(
+      seq('if', $._indent, $._eol, $._block_body, $._dedent, $._eol),
+      seq('if', field('expression', $._full_expression))),
+
+    // Statements ---------------------------------------------------------------------
+
+    global_flag: $ => 'global',
+    export_flag: $ => 'export',
+
+    top_definition: $ => seq(optional($.global_flag), optional($.export_flag), $.definition),
+    top_target:     $ => seq(optional($.global_flag), optional($.export_flag), $.target),
+
+    definition: $ => seq(
+      'def', field('pattern', $._def_pattern),
+      '=',   field('block', $.block_expression),
+      $._eol),
+
+    target: $ => seq(
+      'target', field('pattern', $._def_pattern),
+      field('extra_terms', optional(seq('\\', $.term_patterns))),
+      '=', field('block', $.block_expression),
+      $._eol),
+
+    topic: $ => seq(
+      'topic',  $.low_identifier,
+      ':',      $._full_type,
+      $._eol),
+
+    publish: $ => seq(
+      'publish', $.low_identifier,
+      '=',       $.block_expression,
+      $._eol),
+
+    type_params: $ => repeat1($.low_identifier),
+
+    tuple: $ => seq(
+      optional($.global_flag),
+      optional($.export_flag),
+      'tuple',
+      field('name', $.high_identifier),
+      field('params', optional($.type_params)),
+      '=',
+      $._indent,
+      field('fields', $.fields),
+      $._dedent, $._eol),
+
+    fields: $ => repeat1($.field),
+    field: $ => seq(
+      $._eol,
+      optional($.global_flag),
+      optional($.export_flag),
+      field('name', $.high_identifier),
+      ':',
+      field('type', $._full_type)),
+
+    // data type definitions do not allow recursion:
+    _data_type: $ => choice(
+      $.high_identifier,
+      alias($.data_application_type, $.application_type),
+      alias($.data_binary_type, $.binary_type),
+      alias($.data_unary_type, $.unary_type)),
+    data_application_type: $ => seq($.high_identifier, repeat1($.low_identifier)),
+    data_binary_type: $ => binary_ops($, $.low_identifier),
+    data_unary_type: $ => unary_ops($, $.low_identifier),
+
+    constructors: $ => repeat1(seq($._eol, $._full_type)),
+    constructor1: $ => $._full_type,
+    data: $ => seq(
+      field('global', optional($.global_flag)),
+      field('export', optional($.export_flag)),
+      'data', field('type', $._data_type),
+      '=', field('constructors', choice(
+        seq($._indent, $.constructors, $._dedent),
+        alias($.constructor1, $.constructors))),
+      $._eol),
+
+    // Packages -----------------------------------------------------------------------
+
+    package: $ => seq('package', field('name', $.low_identifier), $._eol),
+
+    def_kind:   $ => 'def',
+    type_kind:  $ => 'type',
+    topic_kind: $ => 'topic',
+    _kind: $ => choice($.def_kind, $.type_kind, $.topic_kind),
+
+    unary_arity:  $ => 'unary',
+    binary_arity: $ => 'binary',
+    _arity: $ => choice($.unary_arity, $.binary_arity),
+
+    ideq: $ => seq(
+      field('to', $.low_identifier),
+      optional(seq('=', field('from', $.low_identifier)))),
+    ideqs: $ => repeat1($.ideq),
+
+    import: $ => seq(
+      'from', 
+      field('package', $.low_identifier),
+      'import',
+      field('kind', optional($._kind)),
+      field('arity', optional($._arity)),
+      field('ideqs', choice($.hole, $.ideqs)),
+      $._eol), 
+
+    export: $ => seq(
+      'from', 
+      field('package', $.low_identifier),
+      'export',
+      field('kind', $._kind),
+      field('arity', optional($._arity)),
+      field('ideqs', $.ideqs),
+      $._eol),
+
+    // Lexer --------------------------------------------------------------------------
+
+    _literal: $ => choice($.integer, $.double, $.regexp, $.string, $.here),
+
+    here: $ => 'here',
+
+    integer: $ => {
+      const decimal = '([1-9][0-9_]*)';
+      const octal   = '(0[0-7_]*)';
+      const hex     = '(0x[0-9a-fA-F_]+)';
+      const binary  = '(0b[01_]+)';
+      return new RegExp([decimal, octal, hex, binary].join('|'));
+    },
+
+    double:  $ => {
+      const double10  = '([0-9][0-9_]*\.[0-9_]+([eE][+-]?[0-9_]+)?)';
+      const double10e = '([0-9][0-9_]*[eE][+-]?[0-9_]+)';
+      const double16  = '(0x[0-9a-fA-F_]+\.[0-9a-fA-F_]+([pP][+-]?[0-9a-fA-F_]+)?)';
+      const double16e = '(0x[0-9a-fA-F_]+[pP][+-]?[0-9a-fA-F_]+)';
+      return new RegExp([double10, double10e, double16, double16e].join('|'));
+    },
+    regexp:  $ => /`[a-z]`/,
+    string:  $ => /"[a-z]"/,
+    // !!!:
+    dot_op:   $ => '.',
+    comma_op: $ => ',',
+    plus_op:  $ => /[-+]/,
+    mul_op:   $ => '*',
+
+    hole: $ => '_',
+    low_identifier:  $ => /[a-z][a-zA-Z0-9_]*/,
+    high_identifier: $ => /[A-Z][a-zA-Z0-9_]*/,
+    _identifier: $ => choice($.low_identifier, $.high_identifier),
+  }
 });
---
+++ src/scanner.c
@@ -0,0 +1,108 @@
+#include <string.h>
+#include <stdlib.h>
+#include <tree_sitter/parser.h>
+
+enum TokenType {
+  EOL,
+  INDENT,
+  DEDENT
+};
+
+// No more than 32 levels deep or 100 characters total.
+struct Indentation {
+  unsigned char depth;
+  unsigned char stack[32];
+  int32_t tabkind[100];
+};
+
+void *tree_sitter_wake_external_scanner_create() {
+  struct Indentation *i = malloc(sizeof(struct Indentation));
+  i->depth = 0;
+  return i;
+}
+
+void tree_sitter_wake_external_scanner_destroy(void *payload) {
+  free(payload);
+}
+
+unsigned tree_sitter_wake_external_scanner_serialize(void *payload, char *buffer) {
+  struct Indentation *i = payload;
+  unsigned out = i->depth;
+  memcpy(buffer, &i->stack[0], out);
+  return out;
+}
+
+void tree_sitter_wake_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
+  struct Indentation *i = payload;
+  i->depth = length;
+  memcpy(&i->stack[0], buffer, length);
+}
+
+static bool is_lws(int32_t c) {
+  return c == ' ' || c == '\t' || c == 0xa0 || c == 0x1680 ||
+         (c >= 0x2000 && c <= 0x200a) ||
+         c == 0x202f || c == 0x205f || c == 0x3000;
+}
+static bool is_nl(int32_t c) {
+  return c == '\n' || c == '\v' || c == '\f' || c == '\r' ||
+         c == 0x85 || c == 0x2028 || c == 0x2029;
+}
+
+bool tree_sitter_wake_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
+  struct Indentation *i = payload;
+
+  // By default, consume nothing.
+  // This is necessary to make it possible to produce INDENT/DEDENT out of nothing.
+  lexer->mark_end(lexer);
+
+  // Skip any trailing whitespace on the line
+  while (is_lws(lexer->lookahead)) lexer->advance(lexer, true);
+
+empty:
+  // Skip any comments
+  if (lexer->lookahead == '#') {
+    do lexer->advance(lexer, true);
+    while (lexer->lookahead && !is_nl(lexer->lookahead));
+  }
+
+  // If we there is still stuff on the current line, there is no EOL/INDENT/DEDENT.
+  if (!is_nl(lexer->lookahead)) return false;
+
+  // Skip the leading whitespace
+  do lexer->advance(lexer, true);
+  while (is_lws(lexer->lookahead));
+
+  // Allow empty lines
+  if (is_nl(lexer->lookahead) || lexer->lookahead == '#') goto empty;
+
+  // How deeply indented are we?
+  unsigned newdepth = lexer->get_column(lexer);
+
+  // How deeply indented WERE we?
+  unsigned olddepth = i->depth ? i->stack[i->depth-1] : 0;
+
+  if (valid_symbols[INDENT] && newdepth > olddepth && newdepth < 256) {
+    if (i->depth < sizeof(i->stack)) {
+      i->stack[i->depth] = newdepth;
+      ++i->depth;
+      lexer->result_symbol = INDENT;
+      return true;
+    } else {
+      // forbid too many levels of nested scope
+      return false;
+    }
+  }
+
+  if (valid_symbols[DEDENT] && newdepth < olddepth) {
+    --i->depth;
+    lexer->result_symbol = DEDENT;
+    return true;
+  }
+
+  if (valid_symbols[EOL] && newdepth == olddepth) {
+    lexer->mark_end(lexer);
+    return true;
+  }
+
+  return false;
+}
